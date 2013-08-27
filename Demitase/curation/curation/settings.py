8f7115dac89c7cc27c30d8298c7f5b9300c74700
masunghoon <masunghoon@gmail.com>
2013-08-27 15:31:30 +0900

    20130826_MA_2

--- Demitase/curation/curation/settings.py
+++ Demitase/curation/curation/settings.py
@@ -15,7 +15,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/fivestar/Fivestar/redcabinet/Demitase/curation/db/db.sqlite3',                      # Or path to database file if using sqlite3.
+#        'NAME': '/home/fivestar/Fivestar/redcabinet/Demitase/curation/db/db.sqlite3',                      # Or path to database file if using sqlite3.
+        'NAME': '/Users/massinet/Workspace/redcabinet/Demitase/curation/db/db.sqlite3',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -160,8 +161,9 @@ LOGGING = {
 }
 
 
-CONSUMER_KEY = 'QN5inGo2xmA7717qxCQ'
-CONSUMER_SECRET = 'dxOvz4PmcWBrQlEkFpK9lu68dT1q0QNEB4UVMYWpmRI'
-CALLBACK_URL = 'http://www.demitase.com/oauth/get_callback/'
-#CONSUMER_KEY = 'FL686aqs4NVkWyrrxs7zQ'
-#CONSUMER_SECRET = 'bxcIpVfAf8OMhKmj966C1EynevAz5OvoLYNlMEYxFE'
+# CONSUMER_KEY = 'QN5inGo2xmA7717qxCQ'
+# CONSUMER_SECRET = 'dxOvz4PmcWBrQlEkFpK9lu68dT1q0QNEB4UVMYWpmRI'
+#CALLBACK_URL = 'http://www.demitase.com/oauth/get_callback/'
+CALLBACK_URL = 'http://127.0.0.1:8000/oauth/get_callback/'
+CONSUMER_KEY = 'FL686aqs4NVkWyrrxs7zQ'
+CONSUMER_SECRET = 'bxcIpVfAf8OMhKmj966C1EynevAz5OvoLYNlMEYxFE'
